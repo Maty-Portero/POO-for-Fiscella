40b9ac149285c2af9cfecfb1f9759986c64d5de4
unknown <porteromatias@gmail.com>
2024-08-09 14:58:14 -0300

    ejer 1 obligatorio listo

---
+++ Ejers Obligatorios/ej1/Cuenta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej1
+{
+    internal class Cuenta
+    {
+        string titular;
+        double saldo;
+
+        public double Saldo 
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+        public string Titular
+        {
+            get { return titular; }
+            set { titular = value; }
+        }
+
+        public Cuenta(string titular, double saldo)
+        {
+            this.Titular = titular;
+            this.Saldo = saldo;
+        }
+        public Cuenta(string titular)
+        {
+            this.Titular = titular;
+        }
+
+        public double Ingresar(double cantidad)
+        {
+            if (cantidad > 0)
+            {
+                Saldo += cantidad;
+            }
+            return Saldo;
+        }
+
+        public double Retirar(double cantidad)
+        {            
+            if (Saldo - cantidad  < 0)
+            {
+                Saldo = 0;
+            }
+            else
+            {
+                Saldo -= cantidad;
+            }
+
+            return Saldo;
+        }
+    }
+}
---
+++ Ejers Obligatorios/ej1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej1
+{
+    internal class Program
+    {
+        static Cuenta cuk = new Cuenta("cuk", 1000);
+
+        static void Main(string[] args)
+        {
+            int IoR;
+            Console.WriteLine("Ingresar: 1     |Retirar : 2     ");
+            IoR = int.Parse(Console.ReadLine());
+
+            switch (IoR)
+            {
+                case 0:
+                    Console.WriteLine("Ingreso: ");
+                    cuk.Ingresar(double.Parse(Console.ReadLine()));
+                    Console.WriteLine("                             ");
+                    Console.WriteLine(cuk.Titular + " tiene " + cuk.Saldo + ".");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Retiro: ");
+                    cuk.Retirar(double.Parse(Console.ReadLine()));
+                    Console.WriteLine("                             ");
+                    Console.WriteLine(cuk.Titular + " tiene " + cuk.Saldo + ".");
+                    break;
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
