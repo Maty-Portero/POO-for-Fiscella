ed4d12396ac9609edfa92990e9424bd161859b70
Maty-Portero <porteromatias@gmail.com>
2024-08-11 18:20:43 -0300

    esta el 2, solo faltan unas cositas

---
+++ Ejers Obligatorios/ej2/Persona.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej2
+{
+    internal class Persona
+    {
+        string nombre = "";
+        int edad = 0;
+        int DNI = 00000001;
+        char sexo = 'H';
+        double peso = 0;
+        double altura = 0;
+
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public int Edad { get { return edad; } set { edad = value; } }
+        public int Dni { get { return DNI; } }
+        public char Sexo { get { return sexo; } set { sexo = value; } }
+        public double Peso { get { return peso; } set { peso = value; } }
+        public double Altura { get { return altura; } set { altura = value; } }
+        
+        public Persona()
+        {
+            nombre = "roberto";
+            edad = 40;
+            DNI = 28945614;
+            sexo = 'H';
+            peso = 80.5;
+            altura = 1.88;
+        }
+
+        public Persona(string nombre, int edad, char sexo)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            DNI = 28945614;
+            this.sexo = sexo;
+            peso = 80.5;
+            altura = 1.88;
+        }
+
+        public Persona(string nombre, int edad, char sexo, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            generaDNI();
+            this.sexo = sexo;
+            comprobarSexo(sexo);
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public int calcularIMC()
+        {
+            double imc = peso / (altura * 2);
+            if (imc > 20)
+            {
+                return -1;
+            }
+            else if (imc > 25 && imc < 20)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public bool esMayorDeEdad()
+        {
+            if (this.edad > 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool comprobarSexo(char sexo)
+        {
+            if (sexo == 'H')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int generaDNI()
+        {
+            Random aleatorio = new Random();
+            int dni;
+            dni = aleatorio.Next(00000001, 99999999);
+
+            if (dni % 2 == 0)
+            {
+                sexo = 'H';
+            }
+            else
+            {
+                sexo = 'M';
+            }
+
+            return dni;
+        }
+    }
+}
---
+++ Ejers Obligatorios/ej2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i > 3; i++) {
+                string name;
+                Console.WriteLine("Ingrese su nombre: ");
+                name = Console.ReadLine();
+
+                int age;
+                Console.WriteLine("Ingrese su edad: ");
+                age = int.Parse(Console.ReadLine());
+
+                char sex;
+                Console.WriteLine("Ingrese su sexo: ");
+                sex = char.Parse(Console.ReadLine());
+
+                double weight;
+                Console.WriteLine("Ingrese su peso: ");
+                weight = double.Parse(Console.ReadLine());
+
+                double height;
+                Console.WriteLine("Ingrese su altura: ");
+                height = double.Parse(Console.ReadLine());
+
+            //}
+            Persona persona1 = new Persona(name, age, sex, weight, height);
+            Persona persona2 = new Persona(name, age, sex, 59.6, 1.79);
+            //Persona persona3 = new Persona(name, age, sex, weight, height);
+
+            Console.WriteLine("Calculo del IMC:  -1 = peso ideal    |    0 = debajo de su peso ideal   |    1 = sobre peso");
+            Console.WriteLine("La persona 1 esta en " + persona1.calcularIMC());
+            Console.WriteLine("La persona 2 esta en " + persona2.calcularIMC());
+            //Console.WriteLine("La persona 3 esta en " + persona3.calcularIMC());
+
+            Console.WriteLine("¿Es mayor de edad?:    true = si   |   false = no");
+            Console.WriteLine("La persona 1 esta en " + persona1.esMayorDeEdad());
+            Console.WriteLine("La persona 2 esta en " + persona2.esMayorDeEdad());
+            //Console.WriteLine("La persona 3 esta en " + persona3.esMayorDeEdad());
+
+            Console.WriteLine(persona1.Nombre + " tiene " + persona1.Edad + " años, su DNI es " + persona1.generaDNI() + " de sexo " + persona1.Sexo + ", pesa " + persona1.Peso + " kg, " + " y mide " + persona1.Altura + " metros.");
+            Console.WriteLine(persona2.Nombre + " tiene " + persona2.Edad + " años, su DNI es " + persona2.generaDNI() + " de sexo " + persona2.Sexo + ", pesa " + persona2.Peso + " kg, " + " y mide " + persona2.Altura + " metros.");
+        }
+    }
+}
