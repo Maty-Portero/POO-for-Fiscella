ee4def28e989ad1e5a8bb054ed76943223ed7b7f
Maty-Portero <porteromatias@gmail.com>
2024-08-12 14:53:01 -0300

    hice el ej 3 y empiezo el 4

---
+++ Ejers Obligatorios/ej3/Password.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej3
+{
+    internal class Password
+    {
+        int longitud = 8;
+        string contrasenia;
+
+        public int Longitud { get { return longitud; } set { longitud = value; } }
+        public string Contrasenia { get { return contrasenia; } set { contrasenia = value; } }
+
+        public Password()
+        {
+            longitud = 8;
+            contrasenia = "";
+        }
+
+        public Password(int longitud)
+        {
+            this.longitud = longitud;
+            generarPassword();
+        }
+
+        public void generarPassword()
+        {
+
+            //Contrasenia = contra;
+        }
+
+        public bool esFuerte()
+        {
+            int mayusas = 0;
+            int minusas = 0;
+            int numsas = 0;
+
+            for (int i = 0; i < Contrasenia.Length; i++)
+            {
+                if (Contrasenia[i] >= 97 && Contrasenia[i] <= 122)
+                {
+                    minusas++;
+                }
+                else if (Contrasenia[i] >= 65 && Contrasenia[i] <= 90)
+                {
+                    mayusas++;
+                }
+                else
+                {
+                    numsas++;
+                }
+            }
+
+            if (mayusas > 2 && minusas > 1 && numsas > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string GetContrasenia()
+        {
+            return Contrasenia;
+        }
+
+        public int GetLongitud()
+        {
+            return Longitud;
+        }
+
+        public void SetLongitud(int longitud)
+        {
+            this.Longitud = longitud;
+        }
+    }
+}
---
+++ Ejers Obligatorios/ej4/Electrodomestico.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej4
+{
+    internal class Electrodomestico
+    {
+        double precioBase = 100;
+        string color = "blanco";
+        char consumoEnergetico = 'F';
+        double peso = 5;
+
+        public double PrecioBase { get { return precioBase; } set { precioBase = value; } } 
+        public string Color { get { return color; } set { color = value; } }
+        public char ConsumoEnergetico { get { return consumoEnergetico; } set { consumoEnergetico= value; } }
+        public double Peso { get { return peso; } set { peso = value; } } 
+
+
+        public Electrodomestico() { }
+
+        public Electrodomestico(double precioBase, double peso)
+        {
+            precioBase = 100;
+            peso = 5;
+        }
+
+        public Electrodomestico(double precioBase, string color, char consumoEnergetico, double peso)
+        {
+            this.precioBase = precioBase;
+            this.color = color;
+            this.consumoEnergetico = consumoEnergetico;
+            this.peso = peso;
+        }
+
+        public double GetPrecioBase()
+        {
+            return precioBase;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+        public char GetConsumoEnergetico()
+        {
+            return consumoEnergetico;
+        }
+    }
+}
