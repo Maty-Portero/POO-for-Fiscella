430fe4902fd1d86a674188b0e1b9f34ed2ab69d6
Maty-Portero <porteromatias@gmail.com>
2024-09-23 20:30:27 -0300

    agrego cosas del 16

---
+++ Ejers Obligatorios/ej16/AgendaContactos.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej16
+{
+    internal class AgendaContactos
+    {
+        List<Contacto> agendaContactos = new List<Contacto>();
+
+        int tamaño = 10;
+
+        public int Tamaño { get { return tamaño; } set { value = tamaño; } }
+
+        public AgendaContactos() { }
+
+        int contadorContactos = 0;
+
+        public void existeContacto(string nombre, string telefono)
+        {
+            for (int i = 0; i < contadorContactos; i++)
+                if (nombre.Equals(agendaContactos[i].Nombre))
+                    Console.WriteLine("Ya existe un contacto con ese nombre");
+        }
+
+        public void aniadirContacto(string nombre, string telefono)
+        {
+            if (contadorContactos <= 10)
+            {
+                agendaContactos[contadorContactos] = new Contacto(nombre, telefono);
+                contadorContactos++;
+            }
+            else
+                Console.WriteLine("La agenda está llena");
+        }
+
+        public void listarContacto()
+        {
+            if (contadorContactos == 0)
+                Console.WriteLine("No hay contactos");
+            else
+                for (int i = 0; i < contadorContactos; i++)
+                    Console.WriteLine(agendaContactos[i].Nombre + "-" + "Tel.:" + agendaContactos[i].Telefono);
+        }
+
+        public void buscaContacto(string nombre)
+        {
+            bool encontrado = false;
+
+            for (int i = 0; i < contadorContactos; i++)
+                if (nombre.Equals(agendaContactos[i].Nombre))
+                {
+                    Console.WriteLine("Tel.:" + agendaContactos[i].Telefono);
+                    encontrado = true;
+                }
+
+            if (!encontrado)
+            {
+                Console.WriteLine("Contacto Inexistente");
+            }
+        }
+
+        public void eliminarContacto(string nombre, string telefono)
+        {
+            bool encontrado = false;
+
+            if (contadorContactos == 0)
+                Console.WriteLine("No hay contactos");
+            else
+                for (int i = 0; i < contadorContactos; i++)
+                    if (nombre.Equals(agendaContactos[i].Nombre))
+                    {
+                        agendaContactos.Remove(agendaContactos[i]);
+                        encontrado = true;
+                    }
+        }
+
+        public void agendaLlena()
+        {
+            if (contadorContactos >= 10)
+                Console.WriteLine("La agenda esta llena");
+        }
+
+        public void huecosLibres()
+        {
+            int espacioLibre = 10 - contadorContactos;
+
+            Console.WriteLine("Quedan " + espacioLibre + " espacios.");
+        }
+    }
+}
---
+++ Ejers Obligatorios/ej16/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var menus = new Dictionary<string, string[]>
+            {
+                { " Archivo ", new[] { "Nuevo Cliente", "Modificar Cliente", "Listar Clientes", "Salir" } },
+                { " Editar " , new[] { "Nuevo Producto", "Modificar Producto", "Eliminar Producto", "Listar Producto", "Salir" } },
+                { " Empresa ", new[] { "Nueva Empresa", "Modificar Empresa", "Eliminar Empresa", "Listar Empresa", "Salir" } }
+            };
+
+            var menuPrincipal = new MenuPrincipal(menus);
+
+            foreach (var subMenu in menus)
+            {
+                foreach (var opcion in subMenu.Value)
+                {
+                    if (opcion == "Salir")
+                    {
+                        menuPrincipal.AsignarAccion(opcion, () =>
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Saliendo del programa...");
+                            Environment.Exit(0);
+                        });
+                    }
+                    else
+                    {
+                        menuPrincipal.AsignarAccion(opcion, () =>
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"Ejecutando {opcion}");
+                            Console.WriteLine("Presione cualquier tecla para continuar...");
+                            Console.ReadKey();
+                        });
+                    }
+                }
+            }
+
+            var menuNavigator = new MenuNavegacion(menuPrincipal);
+            menuNavigator.Iniciar();
+        }
+    }
+}
